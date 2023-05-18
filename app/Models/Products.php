86013f4c7125be1c00797fca57f65d77a03c32fa
kaneko <mai.kaneko.24@gmail.com>
2023-05-18 12:23:43 +0900

    fix model

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use DateTime;
 
 class Products extends Model
 {
@@ -23,5 +24,48 @@ class Products extends Model
         $products = DB::table('products')->get();
 
         return $products;
+    }
+
+    public function registArticle($data, $img) {
+        // 登録処理
+        DB::table('products')->insert([
+            'product_name' => $data->product_name,
+            'company_id' => $data->company_id,
+            'price' => $data->price,
+            'stock' => $data->stock,
+            'comment' => $data->comment,
+            'img_path' => $img,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+    }
+
+    public function editArticle($data, $img) {
+        // 登録処理
+        DB::table('products')->where('id', $data->id)->update([
+            'product_name' => $data->product_name,
+            'company_id' => $data->company_id,
+            'price' => $data->price,
+            'stock' => $data->stock,
+            'comment' => $data->comment,
+            'img_path' => $img,
+            'updated_at' => new DateTime(),
+        ]);
+    }
+
+    public static function searchArticle($keyword, $manufacturer) {
+        // 検索処理
+
+        $query = Products::query();
+        if(!empty($keyword)) {
+            $query->where('product_name', 'LIKE', "%{$keyword}%");
+        }
+        if(!empty($manufacturer)) {
+            $query->where('company_id', 'LIKE', "{$manufacturer}");
+        }
+        $search = $query->with('companies')->get(); 
+
+        return $search;
+
     }
 }
